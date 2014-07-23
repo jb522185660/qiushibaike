bec395fff4ee42be0d8e8f04bbb24f4fa4b206cb
Singer <singer1026@gmail.com>
2014-07-23 23:05:08 +0800

    Initial Commit

---
+++ qiushibaike/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  qiushibaike
+//
+//  Created by Singer on 14-7-23.
+//  Copyright (c) 2014年 Singer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ qiushibaike/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  qiushibaike
+//
+//  Created by Singer on 14-7-23.
+//  Copyright (c) 2014年 Singer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
